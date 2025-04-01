09589b21b8cab6db67121430dcbb3d4b1947b62f
DeepanshuBharaj <deepanshubharaj6@gmail.com>
2025-04-01 23:38:50 +0530

    two_sum
    using hash map to improve complexity

--- twoSum.py
+++ twoSum.py
@@ -1,11 +1,14 @@
+#time complexity : O(n)
+#space complexity : O(n) as in the worst case we might have to store each number in the dictionary
 class Solution:
-    def twoSum(self,nums,target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i]+nums[j] == target :
-                    return [i, j]
-        # Return an empty list if no solution is found
-        return []
+    def twoSum(self,nums,target): 
+        hash_table={}                    #using hash_map to store nums and improve the complexity of the program
+        for i , num in enumerate(nums):
+            complement=target-num
+            if complement in hash_table:
+                return [hash_table[complement],i]
+            hash_table[num] = i          
+        return [] 
     
 nums=[2,4,7,5]
 target=12
